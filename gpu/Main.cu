ef88cd3c4e56875c9b81adfef7433a614024a34d
GregorySchwing <gschwing@uno.edu>
2020-07-15 20:57:28 -0400

    think thisll work

--- gpu/Main.cu
+++ gpu/Main.cu
@@ -1,26 +1,119 @@
 #include <cuda.h>
+#include <thrust/host_vector.h>
+#include <thrust/device_vector.h>
+#include <thrust/generate.h>
+#include <thrust/sort.h>
+#include <thrust/functional.h>
+#include <cstdlib>
+
+// We'll use a 3-tuple to store our 3d vector type
+// rows, cols, vals
+typedef thrust::tuple<int,int,int> Int3;
+
+struct cmp : public std::binary_function<Int3,Int3,bool>
+{
+    __host__ __device__
+        bool operator()(const Int3& a, const Int3& b) const
+        {
+            if (thrust::get<0>(a) != thrust::get<0>(b))
+                return thrust::get<0>(a) < thrust::get<0>(b);
+            else 
+                return thrust::get<1>(a) < thrust::get<1>(b);
+        }
+};
+
 int main(int argc, char *argv[])
 {
     int N = atoi(argv[1]);
     int numEntries = atoi(argv[2]);
+
+/*
     int column_indices_a[numEntries], row_indices_a[numEntries], values_a[numEntries];
     int * column_indices_a_dev, * row_indices_a_dev, * values_a_dev;
+
+    int column_indices_b[numEntries], row_indices_b[numEntries], values_b[numEntries];
+    int * column_indices_b_dev, * row_indices_b_dev, * values_b_dev;
 
     cudaMalloc( (void**)&column_indices_a_dev, numEntries * sizeof(int) );
     cudaMalloc( (void**)&row_indices_a_dev, numEntries * sizeof(int) );
     cudaMalloc( (void**)&values_a_dev, numEntries * sizeof(int) );
 
-    // fill the arrays ‘a’ and ‘b’ on the CPU
-    for (int i=0; i<numEntries; i++) {
-        column_indices_a[i] = rand() % N;
-        row_indices_a[i] = rand() % N;
-        values_a[i] = rand() % N;
+    cudaMalloc( (void**)&column_indices_b_dev, numEntries * sizeof(int) );
+    cudaMalloc( (void**)&row_indices_b_dev, numEntries * sizeof(int) );
+    cudaMalloc( (void**)&values_b_dev, numEntries * sizeof(int) );
+
+    int trialRow, trialCol;
+    bool empty;
+    for (int i = 0; i < numEntries; i++){
+        do {
+            empty = true;
+            trialCol = std::rand() % N;
+            trialRow = std::rand() %  N;
+            for (int j = 0; j < i; j++){
+                if (row_indices_a[j] == trialRow && column_indices_a[j] == trialCol)
+                    empty = false;
+            }
+        } while (!empty);
+        row_indices_a[i] = trialRow;
+        column_indices_a[i] = trialCol;
+        values_a[i] =  std::rand() % N + 1;
+    }
+
+    for (int i = 0; i < numEntries; i++){
+        do {
+            empty = true;
+            trialCol = std::rand() % N;
+            trialRow = std::rand() %  N;
+            for (int j = 0; j < i; j++){
+                if (row_indices_b[j] == trialRow && column_indices_b[j] == trialCol)
+                    empty = false;
+            }
+        } while (!empty);
+        row_indices_b[i] = trialRow;
+        column_indices_b[i] = trialCol;
+        values_b[i] =  std::rand() % N + 1;
     }
 
     cudaMemcpy(column_indices_a_dev, column_indices_a, numEntries * sizeof(int), cudaMemcpyHostToDevice);
     cudaMemcpy(row_indices_a_dev, row_indices_a, numEntries * sizeof(int), cudaMemcpyHostToDevice);
     cudaMemcpy(values_a_dev, values_a, numEntries * sizeof(int), cudaMemcpyHostToDevice);
 
+    cudaMemcpy(column_indices_b_dev, column_indices_b, numEntries * sizeof(int), cudaMemcpyHostToDevice);
+    cudaMemcpy(row_indices_b_dev, row_indices_b, numEntries * sizeof(int), cudaMemcpyHostToDevice);
+    cudaMemcpy(values_b_dev, values_b, numEntries * sizeof(int), cudaMemcpyHostToDevice);
+*/
+    // generate 16M random numbers on the host
+    thrust::device_vector<int> col_vec(numEntries);
+    thrust::device_vector<int> row_vec(numEntries);
+    thrust::device_vector<int> val_vec(numEntries);
 
+    thrust::device_vector<int> dimensions(numEntries);
+    thrust::device_vector<int> ones(numEntries);
+
+    // fill dimensions with twos
+    thrust::fill(dimensions.begin(), dimensions.end(), N);
+    thrust::fill(ones.begin(), ones.end(), 1);
+
+    thrust::generate(col_vec.begin(), col_vec.end(), rand);
+    thrust::generate(row_vec.begin(), row_vec.end(), rand);
+    thrust::generate(val_vec.begin(), val_vec.end(), rand);
+
+    // compute Y = X mod 2
+    thrust::transform(col_vec.begin(), col_vec.end(), dimensions.begin(), col_vec.begin(), thrust::modulus<int>());
+    thrust::transform(row_vec.begin(), row_vec.end(), dimensions.begin(), row_vec.begin(), thrust::modulus<int>());
+    thrust::transform(val_vec.begin(), val_vec.end(), dimensions.begin(), val_vec.begin(), thrust::modulus<int>());
+    thrust::transform(val_vec.begin(), val_vec.end(), ones.begin(), val_vec.begin(), thrust::plus<int>());
+
+    // METHOD #1
+    // Defining a zip_iterator type can be a little cumbersome ...
+    typedef thrust::device_vector<int>::iterator                     IntIterator;
+    typedef thrust::tuple<IntIterator, IntIterator, IntIterator> IntIteratorTuple;
+    typedef thrust::zip_iterator<IntIteratorTuple>                   Int3Iterator;
+
+    // Now we'll create some zip_iterators for A and B
+    Int3Iterator A_first = thrust::make_zip_iterator(thrust::make_tuple(col_vec.begin(), row_vec.begin(), val_vec.begin()));
+    Int3Iterator A_last  = thrust::make_zip_iterator(thrust::make_tuple(col_vec.end(),   row_vec.end(),   val_vec.end()));
+    //Int3Iterator B_first = thrust::make_zip_iterator(thrust::make_tuple(B0.begin(), B1.begin(), B2.begin()));
+    thrust::sort(A_first, A_last, cmp());
 
 }
