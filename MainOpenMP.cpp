1bebf556aad43d6098480fd102a3ecc734316f94
GregorySchwing <gschwing@uno.edu>
2021-08-05 23:39:29 -0500

    BST

--- MainOpenMP.cpp
+++ MainOpenMP.cpp
@@ -5,6 +5,53 @@
 #include "openmp/ParallelB1.h"
 //#include "gpu/COO.cuh"
 
+
+bool KCoverExists(  std::vector<int> & answer,
+                    ParallelKernelization & sk,
+                    std::shared_ptr<Graph> g,
+                    int k){
+    bool exists;
+    sk.TestAValueOfK(k);
+    sk.PrintS();            
+    std::shared_ptr<Graph> gPrime = 
+        std::make_shared<Graph>(g, sk.GetS()); 
+    std::shared_ptr<ParallelB1> pb1 = 
+        std::make_shared<ParallelB1>(gPrime, 
+                                        k, 
+                                        sk.GetS());
+    exists = pb1->IterateTreeStructure(pb1, answer);
+    if (exists) {
+        std::cout << "solution found" << std::endl;
+        std::cout << "Printing answer :" << std::endl;
+        for (auto & v : answer)
+            std::cout << v << " ";
+        std::cout << std::endl;
+    } else {
+        std::cout << "No solution found" << std::endl;
+    }
+    return exists;
+}
+
+int binarySearch(std::vector<int> & answer, 
+                    int minK, 
+                    int maxK,
+                    ParallelKernelization & sk,
+                    std::shared_ptr<Graph> g){
+    int left = minK;
+    int right = maxK;
+    int m;
+    while (left <= right){
+        m = (left + right)/2;
+        if (KCoverExists(answer, sk, g, m)){
+            return m;
+        } else {
+            left = m + 1;
+        }
+    }
+    return m;
+}
+
+
 int main(int argc, char *argv[])
 {
     /* Num edges, N, N, random entries? */
@@ -26,17 +73,23 @@ int main(int argc, char *argv[])
     int k = 4;
     std::cout << "Building PK" << std::endl;
     ParallelKernelization sk(g, k);
+    int minK = 0;
+    int maxK = g->GetVertexCount();
     for (int i = k; i < g->GetVertexCount(); ++i){
-    // If (noSolutionExists)
+        // If (noSolutionExists)
+        // If (Also clears and sets S if a sol'n could exist)
         if (sk.TestAValueOfK(i))
             continue;
-        else {
-            sk.PrintS();            
-
-            std::shared_ptr<Graph> gPrime = std::make_shared<Graph>(g, sk.GetS()); 
-            std::shared_ptr<ParallelB1> pb1 = std::make_shared<ParallelB1>(gPrime, 
-                                                                        k, 
-                                                                        sk.GetS());
+        else{
+            minK = i;
+            break;
         }
     }
+    std::cout << "Found min K : " << minK << std::endl;
+    std::vector<int> answer;
+    int kCovSize = binarySearch(answer,
+                                minK,
+                                maxK,
+                                sk,
+                                g);
 }
