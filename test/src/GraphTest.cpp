282788e83541ed34d10dba4cf0a191fb1c0e8507
go2432 <go2432@wayne.edu>
2021-08-12 21:01:48 -0500

    Added an induce test

--- test/src/GraphTest.cpp
+++ test/src/GraphTest.cpp
@@ -27,19 +27,19 @@ TEST(InduceGraphTest, InduceGraphTest) {
 	Graph gPrime(g);
 	std::vector<int> mpt;
 	gPrime.InitGPrime(g, mpt);
-	std::vector<int> newRowOff = gPrime.GetNewRowOffRef();
-	std::vector<int> newCols = gPrime.GetNewColRef();
-	std::vector<int> newVals = gPrime.GetNewValRef();
+	std::vector<int> newRowOff = gPrime.GetCSR().GetNewRowOffRef();
+	std::vector<int> newCols = gPrime.GetCSR().GetNewColRef();
+	std::vector<int> newVals = gPrime.GetCSR().GetNewValRef();
 	
 	static const int rowOff[] = {0,1,4,5,7,7,10,14,14,15,16};
 	static const int colInd[] = {1,0,5,6,6,5,6,1,3,8,1,2,3,9,5,6};
 	static const int vals[] = {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1};
 
-	vector<int> testRowOff (rowOff, rowOff + sizeof(rowOff) / sizeof(rowOff[0]) );
-	vector<int> testCols (colInd, colInd + sizeof(colInd) / sizeof(colInd[0]) );
-	vector<int> testVals (colInd, colInd + sizeof(colInd) / sizeof(colInd[0]) );
+	std::vector<int> testRowOff(rowOff, rowOff + sizeof(rowOff) / sizeof(rowOff[0]) );
+	std::vector<int> testCols(colInd, colInd + sizeof(colInd) / sizeof(colInd[0]) );
+	std::vector<int> testVals(vals, vals + sizeof(vals) / sizeof(vals[0]) );
 
 	EXPECT_EQ(newRowOff, testRowOff);	
-	EXPECT_EQ(colInd, testCols);	
-	EXPECT_EQ(vals, testVals);	
+	EXPECT_EQ(newCols, testCols);	
+	EXPECT_EQ(newVals, testVals);	
 }
