e4347aa1ed87b3e6a486a9507f7798af7033c35b
go2432 <go2432@wayne.edu>
2021-08-12 20:51:06 -0500

    test induction

--- test/src/GraphTest.cpp
+++ test/src/GraphTest.cpp
@@ -1,6 +1,6 @@
 #include <gtest/gtest.h>
 #include "Graph.h"
-TEST(GraphTest, GraphTest) {
+TEST(InitGraphTest, InitGraphTest) {
 	COO cycle;
 	cycle.BuildCycleCOO();
 	CSR csr(cycle);
@@ -14,4 +14,32 @@ TEST(GraphTest, GraphTest) {
 	g.InitG(g, evenVertices);
 	int remainingEdges = g.GetEdgesLeftToCover();	
 	EXPECT_EQ(remainingEdges, 0);	
+}
+
+TEST(InduceGraphTest, InduceGraphTest) {
+	COO example;
+	example.BuildTheExampleCOO();
+	CSR csr(example);
+	Graph g(csr);
+	std::vector<int> S;
+	S.push_back(4);
+	g.InitG(g, S);
+	Graph gPrime(g);
+	std::vector<int> mpt;
+	gPrime.InitGPrime(g, mpt);
+	std::vector<int> newRowOff = gPrime.GetNewRowOffRef();
+	std::vector<int> newCols = gPrime.GetNewColRef();
+	std::vector<int> newVals = gPrime.GetNewValRef();
+	
+	static const int rowOff[] = {0,1,4,5,7,7,10,14,14,15,16};
+	static const int colInd[] = {1,0,5,6,6,5,6,1,3,8,1,2,3,9,5,6};
+	static const int vals[] = {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1};
+
+	vector<int> testRowOff (rowOff, rowOff + sizeof(rowOff) / sizeof(rowOff[0]) );
+	vector<int> testCols (colInd, colInd + sizeof(colInd) / sizeof(colInd[0]) );
+	vector<int> testVals (colInd, colInd + sizeof(colInd) / sizeof(colInd[0]) );
+
+	EXPECT_EQ(newRowOff, testRowOff);	
+	EXPECT_EQ(colInd, testCols);	
+	EXPECT_EQ(vals, testVals);	
 }
