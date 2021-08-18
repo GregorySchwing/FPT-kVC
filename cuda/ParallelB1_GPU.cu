b892682a0b80147e74bb7057df04bf4ae6b55746
GregorySchwing <gschwing@uno.edu>
2021-08-17 21:23:30 -0500

    2

--- cuda/ParallelB1_GPU.cu
+++ cuda/ParallelB1_GPU.cu
@@ -58,7 +58,8 @@ __global__ void InduceSubgraph( int numberOfRows,
         printf("Thread %d, row %d", threadIdx.x, iter);
         C_ref[iter][0] = 0;
         C_ref[iter][1] = 0;
-        
+        printf("Thread %d, row %d, old_row_offsets_dev[iter] = %d", threadIdx.x, iter, old_row_offsets_dev[iter]);
+
         int beginIndex = old_row_offsets_dev[iter];
         int endIndex = old_row_offsets_dev[iter+1];
 
