9e4ef5f1b0ed72b55f310323e363401fce839322
GregorySchwing <gschwing@uno.edu>
2021-10-11 13:10:15 -0500

    te

--- cuda/ParallelB1_GPU.cu
+++ cuda/ParallelB1_GPU.cu
@@ -53,6 +53,40 @@ __host__ __device__ long long CalculateDeepestLevelWidth(int deepestLevelSize){
     return summand;
 }
 
+__host__ __device__ void PrintStuff(int levelOffset,
+                                    int levelUpperBound,
+                                    int numberOfRows,
+                                    int numberOfEdgesPerGraph,
+                                    int * global_columns_tree,
+                                    int * global_column_buffer,
+                                    int * printAlt,
+                                    int * printCurr){
+
+    printf("Tree\n");
+    for (int i = 0; i < (levelUpperBound-levelOffset)*numberOfEdgesPerGraph; ++i){
+        printf("%d ",global_columns_tree[i]);
+    }
+    printf("\n");
+
+    printf("Buffer\n");
+    for (int i = 0; i < (levelUpperBound-levelOffset)*numberOfEdgesPerGraph; ++i){
+        printf("%d ",global_column_buffer[i]);
+    }
+    printf("\n");
+
+    printf("Unsorted\n");
+
+    for (int i = 0; i < (levelUpperBound-levelOffset)*numberOfEdgesPerGraph; ++i){
+        printf("%d ",printAlt[i]);
+    }
+    printf("\n");
+
+    printf("Sorted\n");
+    for (int i = 0; i < (levelUpperBound-levelOffset)*numberOfEdgesPerGraph; ++i){
+        printf("%d ",printCurr[i]);
+    }
+    printf("\n");
+}
 
 __host__ __device__ long long CalculateSizeRequirement(int startingLevel,
                                                         int endingLevel){
@@ -1411,33 +1445,17 @@ void CallPopulateTree(int numberOfLevels,
             cudaDeviceSynchronize();
             checkLastErrorCUDA(__FILE__, __LINE__);
 
-            std::cout << "Tree" << std::endl;
-            for (int i = 0; i < (levelUpperBound-levelOffset)*numberOfEdgesPerGraph; ++i){
-                std::cout << global_columns_tree[i] << " ";
-            }
-            std::cout << std::endl;
-
-            std::cout << "Buffer" << std::endl;
-            for (int i = 0; i < (levelUpperBound-levelOffset)*numberOfEdgesPerGraph; ++i){
-                std::cout << global_column_buffer[i] << " ";
-            }
-            std::cout << std::endl;
-
-
             int * printAlt = d_values.Alternate();
-            std::cout << "Unsorted" << std::endl;
-            for (int i = 0; i < (levelUpperBound-levelOffset)*numberOfEdgesPerGraph; ++i){
-                std::cout << printAlt[i] << " ";
-            }
-            std::cout << std::endl;
-
             int * printCurr = d_values.Current();
 
-            std::cout << "Sorted" << std::endl;
-            for (int i = 0; i < (levelUpperBound-levelOffset)*numberOfEdgesPerGraph; ++i){
-                std::cout << printCurr[i] << " ";
-            }
-            std::cout << std::endl;
+            PrintStuff  (levelOffset,
+                        levelUpperBound,
+                        numberOfRows,
+                        numberOfEdgesPerGraph,
+                        global_columns_tree,
+                        global_column_buffer,
+                        printAlt,
+                        printCurr);
         }
         
         cudaDeviceSynchronize();
