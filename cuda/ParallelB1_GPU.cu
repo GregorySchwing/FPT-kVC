05507785c640cc909f8542d2f4b5341e21b9d416
GregorySchwing <gschwing@uno.edu>
2021-12-02 14:02:06 -0600

    A

--- cuda/ParallelB1_GPU.cu
+++ cuda/ParallelB1_GPU.cu
@@ -1650,6 +1650,7 @@ __global__ void ParallelAssignMISToNodesBreadthFirst(int * global_active_leaf_in
         dispFromLeft = index - leftMostChildOfLevel*6;
         if (blockIdx.x == 0){
             printf("thread %d index %d\n",threadIdx.x, index);
+            printf("thread %d (float)(index/2) %f", threadIdx.x, (float)(index/2));
             printf("thread %d (float)(index/2 + index == 0 + index == 1) %f\n",threadIdx.x, (float)(index/2 + index == 0 + index == 1));
             printf("thread %d logf((float)(index/2 + index == 0 + index == 1)) %f\n",threadIdx.x, logf((float)(index/2 + index == 0 + index == 1)));
             printf("thread %d logf((float)(index/2 + index == 0 + index == 1) / logf(3)) %f\n",threadIdx.x, logf((float)(index/2 + index == 0 + index == 1)) / logf(3));
