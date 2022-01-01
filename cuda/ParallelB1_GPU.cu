6271f9cb646e2595d9760da768c5972c7dbd4d3e
GregorySchwing <gschwing@uno.edu>
2021-12-31 19:22:01 -0600

    A

--- cuda/ParallelB1_GPU.cu
+++ cuda/ParallelB1_GPU.cu
@@ -1800,7 +1800,7 @@ __global__ void ParallelAssignMISToNodesBreadthFirstClean(int * global_active_le
         levelDepth += (int)(relativeLeafIndex / 3279 != 0);
         leftMostChildOfLevel = ((2*arbitraryParameter+3)*powf(3.0, levelDepth) - 3)/6;
         leftMostChildOfLevelExpanded = 2*leftMostChildOfLevel-1;
-        dispFromLeft = index - powf(3.0, levelDepth)*(int)(levelDepth!=0);
+        dispFromLeft = index % 2*powf(3.0, levelDepth+1);
         if (blockIdx.x == 0){
             printf("thread %d index %d\n",threadIdx.x, index);
             printf("thread %d pathIndex %d\n", threadIdx.x, pathIndex);
