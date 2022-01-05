e97265bc131281f7e64c554d079cd2dab7beae3e
GregorySchwing <gschwing@uno.edu>
2022-01-05 16:06:27 -0600

    writing set inclusion bools to global.

--- cuda/ParallelB1_GPU.cu
+++ cuda/ParallelB1_GPU.cu
@@ -1904,7 +1904,7 @@ __global__ void ParallelIdentifyVertexDisjointNonPendantPathsClean(
         cardinalityOfV = pathsAndIndependentStatus[setReductionOffset];
         ++seed;
     }
-
+    global_set_inclusion_bool_ptr[globalSetInclusionBoolOffset + threadIdx.x] = pathsAndIndependentStatus[setInclusionOffset + threadIdx.x];
     pathsAndIndependentStatus[setReductionOffset + threadIdx.x] = pathsAndIndependentStatus[setInclusionOffset + threadIdx.x];
     int i = blockDim.x/2;
     __syncthreads();
